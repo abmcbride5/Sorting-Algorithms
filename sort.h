d3c26eed5bc537e7f0d6cbc56f08b64e584025eb
Amber <abmcbride5@gmail.com>
2019-06-18 06:47:12 +0000

    updating header

--- sort.h
+++ sort.h
@@ -36,5 +36,6 @@ void cocktail_sort_list(listint_t **list);
 void merge_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 
 #endif
